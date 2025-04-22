479bc7a4cba1d1ac69a5211a272ec614508c31e0
Adool <adelsaid284@gmail.com>
2025-04-22 16:23:08 +0200

    edit home page

--- lib/View/widgets/notes_view_body.dart
+++ lib/View/widgets/notes_view_body.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:notetest/View/widgets/custom_app_bar.dart';
 
 class NotesViewBody extends StatelessWidget {
@@ -8,7 +9,46 @@ class NotesViewBody extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: EdgeInsets.symmetric(horizontal: 24),
-      child: Column(children: [const SizedBox(height: 30), CustomAppBar()]),
+      child: Column(
+        children: [const SizedBox(height: 30), CustomAppBar(), NoteItem()],
+      ),
+    );
+  }
+}
+
+class NoteItem extends StatelessWidget {
+  const NoteItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Color(0xffFFCC80),
+        borderRadius: BorderRadius.circular(16),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          ListTile(
+            title: const Text(
+              "Flutter Tips",
+              style: TextStyle(color: Colors.black, fontSize: 26),
+            ),
+            subtitle: Text(
+              "Build career with Adel",
+              style: TextStyle(
+                color: Colors.black.withValues(alpha: .5),
+                fontSize: 20,
+              ),
+            ),
+            trailing: IconButton(
+              onPressed: () {},
+              icon: Icon(FontAwesomeIcons.trash, size: 28, color: Colors.black),
+            ),
+          ),
+          Text("May21 ,2022", style: TextStyle(color: Colors.black)),
+        ],
+      ),
     );
   }
 }
