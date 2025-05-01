ce1ada7de4d5aad7767837e5fed7335cd736cea5
Adool <adelsaid284@gmail.com>
2025-05-01 20:49:10 +0300

    add note logic

--- lib/cubits/AddNote/add_note_cubit.dart
+++ lib/cubits/AddNote/add_note_cubit.dart
@@ -1,4 +1,6 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hive_flutter/adapters.dart';
+import 'package:notetest/constant.dart';
 import 'package:notetest/model/note_model.dart';
 
 part 'add_note_state.dart';
@@ -6,8 +8,17 @@ part 'add_note_state.dart';
 class AddNoteCubit extends Cubit<AddNoteState> {
   AddNoteCubit(super.initialState);
 
-  addNote(NoteModel note){
-     //to save in database
-  }
+  addNote(NoteModel note) async {
+    //to save in database
+    emit(AddNoteLoading());
 
+    try {
+      var notesBox = Hive.box<NoteModel>(knotesBox);
+      await notesBox.add(note);
+      emit(AddNoteSuccess());
+    } on Exception catch (e) {
+      emit(AddNoteFailure(e.toString()));
+    }
+    // add  accept dynamic data type, befire it must do register adapter
+  }
 }
