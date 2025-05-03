6cee3d80ca5e7da6b7e3a141c1b819aa90503add
Adool <adelsaid284@gmail.com>
2025-05-03 15:11:44 +0300

    delete note

--- lib/View/widgets/notes_view_body.dart
+++ lib/View/widgets/notes_view_body.dart
@@ -72,7 +72,10 @@ class NoteItem extends StatelessWidget {
                 ),
               ),
               trailing: IconButton(
-                onPressed: () {},
+                onPressed: () {
+                  noteModel.delete();
+                 
+                },
                 icon: const Icon(
                   FontAwesomeIcons.trash,
                   size: 24,
